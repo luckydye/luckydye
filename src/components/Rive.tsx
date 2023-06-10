da6d88ac98b84c41e815fc152af5dc208b717723
Tim Havlicek <t.havlicek@s-v.de>
2023-06-10 18:02:41 +0200

    rive animation

--- src/components/Rive.tsx
+++ src/components/Rive.tsx
@@ -1,21 +1,27 @@
-import riveWASMResource from '@rive-app/canvas/rive.wasm';
-import { Rive, RuntimeLoader } from '@rive-app/canvas';
-import {createSignal } from "solid-js";
+import riveWASMResource from "@rive-app/canvas/rive.wasm?url";
+import { Rive, RuntimeLoader } from "@rive-app/canvas";
+import { createSignal } from "solid-js";
 
 RuntimeLoader.setWasmUrl(riveWASMResource);
 
-export default () => {
+export default ({ src, width, height }) => {
+  const canvas = document.createElement("canvas");
+  canvas.width = width * window.devicePixelRatio;
+  canvas.height = height * window.devicePixelRatio;
 
+  canvas.style.width = width + "px";
+  canvas.style.height = height + "px";
 
-  const riveInstance = new Rive({
+  const [loaded, setLoaded] = createSignal(false);
+
+  new Rive({
     src: src,
-    ...
+    canvas: canvas,
+    autoplay: true,
+    onLoad() {
+      setLoaded(true);
+    },
   });
 
-  return (
-    <div>
-      <div>Count value in solid is {count()}</div>
-      <div>Also btw vue says: {globalCount()}</div>
-    </div>
-  );
+  return <div class={`${!loaded ? "hidden" : ""}`}>{canvas}</div>;
 };

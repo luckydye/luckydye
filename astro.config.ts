ead27d0b1da9569aef66b9bfa8a1873992c324b1
Tim Havlicek <t.havlicek@s-v.de>
2023-07-31 14:04:12 +0200

    test

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import solid from "@astrojs/solid-js";
+import vercelEdge from "@astrojs/vercel/edge";
 
 export default defineConfig({
+  output: "server",
+  adapter: vercelEdge(),
   base: "",
   experimental: {
     assets: true,

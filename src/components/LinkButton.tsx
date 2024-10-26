a591e661edde89a2a2c1555f6bf8eca58b992ad2
Tim Havlicek <tim.h4vlicek@gmail.com>
2024-10-26 09:05:24 +0200

    Update LinkButton.tsx

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -3,7 +3,7 @@ export function LinkButton(props: { url: string; children: any }) {
     <a
       target="_blank"
       href={props.url}
-      class="text-base transition-all active:bg-inherit px-4 py-2 rounded-md bg-zinc-800 hover:bg-zinc-700 border border-zinc-700"
+      class="text-base transition-all active:bg-inherit px-4 py-2 hover:bg-zinc-900 border border-zinc-700"
       rel="noreferrer"
       style="text-decoration: none;"
     >

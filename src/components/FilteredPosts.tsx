40eb95be19fb402a90a7390b8d4744a09d91f43b
Tim Havlicek <tim.h4vlicek@gmail.com>
2026-01-31 16:30:22 +0100

    Remove filter form from FilteredPosts component
    Removed filter form and related elements from FilteredPosts component.

--- src/components/FilteredPosts.tsx
+++ src/components/FilteredPosts.tsx
@@ -27,36 +27,7 @@ export function FilteredPosts(props: {
   });
 
   return (
-    <div>
-      <div class="relative z-50 flex flex-wrap items-center gap-12 pt-12 pb-20">
-        <h2 class="my-2 text-3xl leading-tight">Work</h2>
-
-        <form
-          class="flex min-w-[320px] flex-1 flex-wrap justify-center gap-4 md:pr-24"
-          onChange={(ev) => {
-            const data = new FormData(ev.currentTarget);
-            setFilter(
-              [...data].filter((value) => value[1] === "true").map((value) => value[0]),
-            );
-          }}
-        >
-          {props.tags.map((t) => {
-            return (
-              <a-toggle
-                key={t.id}
-                name={t.id}
-                value={filter().includes(t.id)}
-                class="group cursor-pointer whitespace-nowrap rounded-full focus-within:ring-2"
-              >
-                <span class="block rounded-full bg-fg-1 px-5 py-2 text-[1.125rem] text-white leading-[1.75rem] group-hover:bg-zinc-700 group-active:scale-[0.985] group-[&[value='true']]:bg-zinc-600 group-hover:group-[&[value='true']]:bg-zinc-500">
-                  {t.title}
-                </span>
-              </a-toggle>
-            );
-          })}
-        </form>
-      </div>
-
+    <div class="pt-10">
       <div class="grid grid-cols-8 gap-y-10 sm:gap-x-20">
         {posts().length > 0 ? posts() : <p>No posts found</p>}
       </div>
